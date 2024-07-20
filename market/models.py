e5b07a8053b458677c12f56e0e885b9071b8f5b1
Felix Kipkorir <kipronofelix63@gmail.com>
2024-07-21 02:12:19 +0300

    edited password_hash attribute from int to string

--- market/models.py
+++ market/models.py
@@ -25,7 +25,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(20), nullable=False, unique=True)
-    password_hash = db.Column(db.Integer(), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=5000)
     items = db.relationship('Item', backref='owner', lazy=True)
 
