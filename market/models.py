0b6bb57f54647b7f24bd99de8fe041f638127a41
Felix Kipkorir <kipronofelix63@gmail.com>
2024-07-23 22:41:32 +0300

    modified email to accept more characters

--- market/models.py
+++ market/models.py
@@ -24,7 +24,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
-    email = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=5000)
     items = db.relationship('Item', backref='owner', lazy=True)
